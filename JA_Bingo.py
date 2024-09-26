75915bd0e1f831f37b2b5fad6de8972971c0371a
Nilodirf <106656383+Nilodirf@users.noreply.github.com>
2024-09-26 19:46:52 +0200

    Create JA_Bingo.py

---
+++ JA_Bingo.py
@@ -0,0 +1,83 @@
+import numpy as np
+
+def get_string(i):
+    if i == 0:
+        return 'Vieze-sueddeutsche Meisterin im Rollkunstlauf 2001'
+    if i == 1:
+        return 'Hauttyp 4'
+    if i == 2:
+        return 'Leseratte mit gesteigertem Interesse an Knochen'
+    if i == 3:
+        return 'Lieblingsessen der Kindheit sind Hefekloesse'
+    if i == 4:
+        return 'Marathon', 'trockener Rotwein'
+    if i == 5:
+        return 'Voltigiert', 'fast ein Christkind'
+    if i == 6:
+        return 'Spielt Geige', 'sehr ruhig'
+    if i == 7:
+        return 'Spielt in einer Band', 'Lieblingspferd heisst Dancer'
+    if i == 8:
+        return 'Hat eine Orgel zu Hause'
+    if i == 9:
+        return 'Darf Motorradfahren', 'Isst gerne Latritz'
+    if i == 10:
+        return 'Hat 11 Geschwister'
+    if i == 11:
+        return 'Am liebsten mache ich Orlaub', 'War lange Zeit ein Rauchi'
+    if i == 12:
+        return 'Hat mehr als 250 Brettspiele'
+    if i == 13:
+        return 'War in zwei verschiedenen Choeren mit Jana'
+    if i == 14:
+        return 'Hat in 4 Laendern gelebt'
+    if i == 15:
+        return 'Hat ein einem Film mit Keanu Reeves gespielt'
+    if i == 16:
+        return 'Hat drei kleine Zehen'
+    if i == 17:
+        return 'Phobie vor Bohren'
+    if i == 18:
+        return 'Hat schon einmal Froesche gegessen', 'Schwerlast-/ und Deckenkrahnfuehrerschein'
+    if i == 19:
+        return 'Hat frueher mit Alex heimlich Haendchen gehalten'
+    if i == 20:
+        return 'Opa war DDR Skat-Turnier Spieler'
+    if i == 21:
+        return 'Hat zwei Monate wild in den Bergen gelebt'
+    if i == 22:
+        return 'Ist bei der Pilotenausbildungsaufnahmepruefung durchgefallen'
+    if i == 23:
+        return 'Geht seit 14 Jahren auf dasselbe Reggea-Festival'
+    if i == 24:
+        return 'Wollte als Kind Muellsammler werden'
+    if i == 25:
+        return 'fehlt Em'
+
+
+Vieze-sueddeutsche Meisterin im Rollkunstlauf 2001
+Hauttyp 4
+Leseratte mit gesteigertem Interesse an Knochen
+Lieblingsessen der Kindheit sind Hefekloesse
+Marathon, trockener Rotwein
+Voltigiert, fast ein Christkind
+Spielt Geige, sehr ruhig
+Spielt in einer Band, Lieblingspferd heisst Dancer
+Hat eine Orgel zu Hause
+DarfMotorradfahren, Isst gerne Latritz
+Hat 11 Geschwister
+Am liebsten mache ich Orlaub, War lange Zeit ein Rauchi
+Hat mehr als 250 Brettspiele
+War in zwei verschiedenen Choeren mit Jana
+Hat in 4 Laendern gelebt
+Hat ein einem Film mit Keanu Reeves gespielt
+Hat drei kleine Zehen
+Phobie vor Bohren
+Hat schon einmal Froesche gegessen, Schwerlast-/ und Deckenkrahnfuehrerschein
+Hat frueher mit Alex heimlich Haendchen gehalten
+Opa war DDR Skat-Turnier Spieler
+Hat zwei Monate wild in den Bergen gelebt
+Ist bei der Pilotenausbildungsaufnahmepruefung durchgefallen
+Geht seit 14 Jahren auf dasselbe Reggae-Festival
+Wollte als Kind Muellsammler werden
+fehlt Em
